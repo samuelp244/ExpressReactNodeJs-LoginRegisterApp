780a4aaf6e7f6c2233cd142cd9fecedd1b606b36
samuelp244 <samuelp244@gmail.com>
2022-07-15 17:08:43 +0530

    database connection established successfully!

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"Sam012345",
+    database:"users_db"
+});
 
 app.get("/hello",(req,res)=>{
-    res.send("hii")
+    db.query("INSERT INTO teachers (username,password) VALUES ('sam','01234')", 
+    (err,result)=>{
+        if(err) {
+            console.log(err)
+        }
+        res.send(result)
+    })
 })
 
 app.listen(3001,()=>{
